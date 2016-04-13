96015804daf45502ba3559192dd2ac4af83f65a2
mlabouardy <mohamed@labouardy.com>
2016-04-13 12:30:29 +0100

    Create docker swarm manager

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+n=6;
+driver="virtualbox";
+
 echo "Check if docker-machine is installed";
+
+for ((i=1; i<=$n; i++)); do
+	echo "Creating node $i";
+	docker-machine create --driver $driver node-$i --swarm --swarm-master --swarm-discovery=token://$token;
+	docker-machine create --driver $driver node-$i --swarm --swarm-addr=tcp://$ip:2375 --swarm-discovery=token://$token;
+done
+
+$SHELL
