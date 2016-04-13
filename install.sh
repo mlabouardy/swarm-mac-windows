87fc071143a47c4b97abcaf12a037ea18dd1713f
mlabouardy <mohamed@labouardy.com>
2016-04-13 12:42:54 +0100

    set cluster nodes and docker-machine driver

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-n=6;
-driver="virtualbox";
+# Set Cluster nodes number
+if [ $?CLUSTER_NODES != 1 ]; then 
+    CLUSTER_NODES=6;
+fi
 
-echo "Check if docker-machine is installed";
-if ![type docker-machine >/dev/null]; then
+# Set docker-machine create driver
+if [ $?CLUSTER_DRIVER != 1 ]; then 
+    CLUSTER_DRIVER="virtualbox";
+fi
+
+# Check if docker-machine is installed
+if ! type docker-machine >/dev/null; then
     "Docker-machine is required.  Aborting."
     exit 1;
 fi
 
-for ((i=1; i<=$n; i++)); do
+# Create nodes cluster
+for ((i=1; i<=$CLUSTER_NODES; i++)); do
 	echo "Creating node $i";
-	docker-machine create --driver $driver node-$i --swarm --swarm-master --swarm-discovery=token://$token;
-	docker-machine create --driver $driver node-$i --swarm --swarm-addr=tcp://$ip:2375 --swarm-discovery=token://$token;
+	docker-machine create --driver $CLUSTER_DRIVER node-$i --swarm --swarm-master --swarm-discovery=token://$token;
+	docker-machine create --driver $CLUSTER_DRIVER node-$i --swarm --swarm-addr=tcp://$ip:2375 --swarm-discovery=token://$token;
 done
 
 $SHELL
